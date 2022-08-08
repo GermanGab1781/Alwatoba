d0d868423bc7472340e0f5e3aa2bd6dad2d287dc
unknown <germangabriel1998@gmail.com>
2022-08-08 13:11:31 -0300

    Home page big screens breakpoint finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,14 +12,14 @@ export default function Navbar(){
   const [navButton, setNavButton] = useState(true);
   return (
     <motion.div className={navButton 
-      ?"sm:bg-amber-200 md:bg-blue-800 lg:bg-amber-700 bg-green-400 grid grid-rows-1 grid-cols-10 content-center fixed z-50 w-screen h-10 p-8 pt-8 transition-all ease-in-out duration-1000 delay-75" 
-      :"sm:bg-amber-200 md:bg-blue-800 lg:bg-amber-700 bg-green-400 grid grid-rows-1 grid-cols-10 content-center fixed z-50 w-screen h-screen p-8 pt-8 transition-all ease-in-out duration-1000"}
+      ?"sm:bg-amber-200 md:bg-blue-800 lg:bg-amber-700 bg-green-400 grid grid-rows-1 grid-cols-9 content-center fixed z-50 w-screen h-10 p-8 pt-8 transition-all ease-in-out duration-1000 delay-75" 
+      :"sm:bg-amber-200 md:bg-blue-800 lg:bg-amber-700 bg-green-400 grid grid-rows-1 grid-cols-9 content-center fixed z-50 w-screen h-screen p-8 pt-8 transition-all ease-in-out duration-1000"}
     animate={{opacity:1}} exit={{opacity:0}}>
       
       {/* img Brand */}
       <NavLink onClick={()=>setNavButton(true)} className="sm:ml-2 sm:col-span-2 ml-5 row-start-0 row-end-1 col-start-1 col-span-3" to="/Alwatoba"><img className="sm:h-10 sm:w-full w-1/2 h-full" src={AlwatobaImg} alt="Marca de la empresa" /></NavLink>
       {/* Botones Derecha */}
-      <div className="grid row-start-0 col-start-10">        
+      <div className="grid row-start-0 sm:col-start-10 col-start-8">        
         <NavLink onClick={()=>setNavButton(true)} className="sm:row-start-1 sm:col-start-1 row-start-1 col-start-1" to="/"><BsFillCartFill className="sm:w-8 sm:h-12 h-10 w-5" /></NavLink>
         <button onClick={()=>setNavButton(!navButton)} className="sm:row-start-1 sm:col-start-2 sm:ml-10 row-start-1 col-start-2 ml-4"><FaBars className="sm:w-10 sm:h-12 h-10 w-8"/></button>      
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,91 @@
 import { motion } from 'framer-motion'
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import ReactPlayer from 'react-player'
 import AlwatobaImg from '../media/AlwatobaBrand.png'
 
 export default function Home() {
   return (
-    <motion.div className="sm:bg-amber-200 md:bg-blue-800 lg:bg-amber-700 bg-green-400 grid grid-rows-6 grid-cols-6 pt-20 place-items-center" initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
+    <motion.div className="sm:bg-amber-200 md:bg-blue-800 lg:bg-amber-700 bg-green-400 grid gap-y-20 grid-cols-6 pt-20  place-items-center" initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
       {/* Presentacion */}
-      <NavLink className="row-start-1 col-start-3 col-span-2" to="/"><img className="w-full h-full" src={AlwatobaImg} alt="Presentacion"/></NavLink>
+      <div className="row-start-1 col-start-3 col-span-2 border border-amber-300 p-5 rounded-2xl">
+        <ReactPlayer width={"70vw"} height={"40vw"} url='https://www.youtube.com/watch?v=cFPkypL_ap8' controls="true" />
+      </div>      
       {/* Frase 1 */}
-      <div className="row-start-2 col-start-3 col-span-2 h-32 w-1/2 relative">
+      <div className="row-start-2 col-start-3 col-span-2 h-32 w-1/2 relative ">
         <img className="h-full w-full rounded-full" src={AlwatobaImg} alt="Imagen fondo"/>
-        <span className="absolute bg-black bottom-1/2 left-1/2 -translate-x-1/2 whitespace-nowrap text-md font-bold text-amber-400">"Conscientes de nuestro impacto en la Cultura"</span>
+        <span className="absolute bg-black bottom-1/3 left-1/2 -translate-x-1/2 whitespace-nowrap text-2xl font-bold text-amber-400">"Conscientes de nuestro impacto en la Cultura"</span>
       </div>
       {/* Categorias Productos */}
-      <div className="row-start-3 bg-black">
-        <div>
-          <img className="h-full w-full rounded-full" src={AlwatobaImg} alt="Imagen de categoria de producto"/>
-          <NavLink to="/Catalog"><span>Carteras</span></NavLink>
+      <div className="flex flex-row justify-center col-span-7 gap-x-12 row-start-3 text-center">
+
+        <div className="relative bg-slate-500">
+          <img className="h-96 w-52" src={AlwatobaImg} alt="Imagen de categoria de producto"/>
+          <NavLink className="absolute bg-slate-500 w-3/4 h-1/6 -bottom-6 left-1/2 -translate-x-1/2" to="/Catalog">
+            <span className="text-amber-400 relative top-2 text-lg font-semibold">Categoria1</span><br/>
+            <span className="text-amber-400 relative top-1 text-sm">mini descripcion </span>       
+          </NavLink>         
+        </div>
+        <div className="relative bg-slate-500">
+          <img className="h-96 w-52" src={AlwatobaImg} alt="Imagen de categoria de producto"/>
+          <NavLink className="absolute bg-slate-500 w-3/4 h-1/6 -bottom-6 left-1/2 -translate-x-1/2" to="/Catalog">
+            <span className="text-amber-400 relative top-2 text-lg font-semibold">Categoria2</span><br/>
+            <span className="text-amber-400 relative top-1 text-sm">mini descripcion </span>       
+          </NavLink>         
+        </div>
+        <div className="relative bg-slate-500">
+          <img className="h-96 w-52" src={AlwatobaImg} alt="Imagen de categoria de producto"/>
+          <NavLink className="absolute bg-slate-500 w-3/4 h-1/6 -bottom-6 left-1/2 -translate-x-1/2" to="/Catalog">
+            <span className="text-amber-400 relative top-2 text-lg font-semibold">Categoria3</span><br/>
+            <span className="text-amber-400 relative top-1 text-sm">mini descripcion </span>       
+          </NavLink>         
+        </div>
+        <div className="relative bg-slate-500">
+          <img className="h-96 w-52" src={AlwatobaImg} alt="Imagen de categoria de producto"/>
+          <NavLink className="absolute bg-slate-500 w-3/4 h-1/6 -bottom-6 left-1/2 -translate-x-1/2" to="/Catalog">
+            <span className="text-amber-400 relative top-2 text-lg font-semibold">Categoria4</span><br/>
+            <span className="text-amber-400 relative top-1 text-sm">mini descripcion </span>       
+          </NavLink>         
+        </div>
+      </div>
+      {/* Frase 2 */}
+      <div className="row-start-4 col-start-3 col-span-2 h-32 w-1/2 relative">
+        <img className="h-full w-full rounded-full" src={AlwatobaImg} alt="Imagen fondo"/>
+        <span className="absolute bg-black bottom-1/3 left-1/2 -translate-x-1/2 whitespace-nowrap text-2xl font-bold text-amber-400">"Trabajando de la mano de las comunidades mapuches"</span>
+      </div>
+      {/* Blogs */}
+      <div className="flex flex-wrap justify-evenly gap-y-10 row-start-5 col-start-2 col-span-4">
+
+        <div className="relative h-52 w-96 border border-black">
+          <img className="h-full w-full" src={AlwatobaImg} alt="Imagen fondo"/>
+          <span className="absolute bottom-6 text-2xl font-semibold whitespace-pre">BLOG EJEMPLO 1</span>
+          <NavLink to="/"><span className="absolute bottom-1 right-1 text-md p-1 rounded-lg border  hover:text-lg border-black transition-all">VER</span></NavLink>
+        </div>
+        <div className="relative h-52 w-96 border border-black">
+          <img className="h-full w-full" src={AlwatobaImg} alt="Imagen fondo"/>
+          <span className="absolute bottom-6 text-2xl font-semibold whitespace-pre">BLOG EJEMPLO 1</span>
+          <NavLink to="/"><span className="absolute bottom-1 right-1 text-md p-1 rounded-lg border  hover:text-lg border-black transition-all">VER</span></NavLink>
+        </div>
+        <div className="relative h-52 w-96 border border-black">
+          <img className="h-full w-full" src={AlwatobaImg} alt="Imagen fondo"/>
+          <span className="absolute bottom-6 text-2xl font-semibold whitespace-pre">BLOG EJEMPLO 1</span>
+          <NavLink to="/"><span className="absolute bottom-1 right-1 text-md p-1 rounded-lg border  hover:text-lg border-black transition-all">VER</span></NavLink>
+        </div>
+        <div className="relative h-52 w-96 border border-black">
+          <img className="h-full w-full" src={AlwatobaImg} alt="Imagen fondo"/>
+          <span className="absolute bottom-6 text-2xl font-semibold whitespace-pre">BLOG EJEMPLO 1</span>
+          <NavLink to="/"><span className="absolute bottom-1 right-1 text-md p-1 rounded-lg border  hover:text-lg border-black transition-all">VER</span></NavLink>
+        </div>
+
+        <div className="relative h-52 w-96 border border-black">
+          <img className="h-full w-full" src={AlwatobaImg} alt="Imagen fondo"/>
+          <span className="absolute bottom-6 text-2xl font-semibold whitespace-pre">BLOG EJEMPLO 1</span>
+          <NavLink to="/"><span className="absolute bottom-1 right-1 text-md p-1 rounded-lg border  hover:text-lg border-black transition-all">VER</span></NavLink>
+        </div>
+        <div className="relative h-52 w-96 border border-black">
+          <img className="h-full w-full" src={AlwatobaImg} alt="Imagen fondo"/>
+          <span className="absolute bottom-6 text-2xl font-semibold whitespace-pre">BLOG EJEMPLO 1</span>
+          <NavLink to="/"><span className="absolute bottom-1 right-1 text-md p-1 rounded-lg border  hover:text-lg border-black transition-all">VER</span></NavLink>
         </div>
       </div>
     </motion.div>

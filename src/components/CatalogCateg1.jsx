22c29aae66c37ef0c56c08e0b7bd414f96fb0b3a
unknown <germangabriel1998@gmail.com>
2022-08-10 20:28:28 -0300

    Finished Catalog Page navbar of subroutes for mobile screens

--- src/components/CatalogCateg1.jsx
+++ src/components/CatalogCateg1.jsx
@@ -5,7 +5,7 @@ import AlwatobaImg from '../media/AlwatobaBrand.png'
 
 export default function CatalogCateg1() {
   return (
-    <motion.div className="flex flex-wrap justify-start gap-y-10 gap-x-5 ml-5 mt-5 max-h-full max-w-full place-content-start" initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
+    <motion.div className="flex flex-wrap gap-y-10 gap-x-5 mt-5 max-h-full max-w-full place-content-center" initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
       <NavLink className="sm:h-48 sm:w-48 h-32 w-32 transition ease-in-out duration-100 hover:scale-105 " to="/Product">
         <img src={AlwatobaImg} className="w-full h-full" alt="Categ1"/>
         <span>PRODUCTO</span>     
@@ -29,7 +29,11 @@ export default function CatalogCateg1() {
       <NavLink className="sm:h-48 sm:w-48 h-32 w-32 transition ease-in-out duration-100 hover:scale-105 " to="/Product">
         <img src={AlwatobaImg} className="w-full h-full" alt="Categ1"/>
         <span>PRODUCTO</span>     
-      </NavLink> 
+      </NavLink>
+      <NavLink className="sm:h-48 sm:w-48 h-32 w-32 transition ease-in-out duration-100 hover:scale-105 " to="/Product">
+        <img src={AlwatobaImg} className="w-full h-full" alt="Categ1"/>
+        <span>PRODUCTO</span>     
+      </NavLink>  
       
       
        
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,26 +1,25 @@
 import React, { useState } from 'react';
 import {motion} from 'framer-motion'
-import { NavLink, Outlet,Link } from 'react-router-dom';
-import AlwatobaImg from '../media/AlwatobaBrand.png'
+import { Outlet,Link } from 'react-router-dom';
 
 export default function Catalog() {
   const [menuButton, setMenuButton] = useState(true)
   return (
-    <motion.div className="bg-amber-300 pt-16 grid grid-cols-8 text-center" initial={{opacity:1}} animate={{opacity:1}} exit={{opacity:1}}>
+    <motion.div className="bg-amber-300 pt-16 pb-10 grid grid-cols-8 text-center" initial={{opacity:1}} animate={{opacity:1}} exit={{opacity:1}}>
         <span className="row-start-1 sm:col-span-8 col-start-3 col-end-8 font-bold text-2xl sm:mt-0 mt-5">
           Nuestros Productos
         </span>
         {/* Menu lateral Izq */}
         <div className={menuButton 
-          ?"sm:relative fixed flex flex-col justify-start bg-amber-200 sm:row-start-2 sm:col-start-1 sm:col-span-1 sm:mt-0 mt-5 sm:w-full w-24 h-full visible transition-all duration-500 "
-          :"sm:relative fixed flex flex-col justify-start bg-amber-200 sm:row-start-2 sm:col-start-1 sm:col-span-1 sm:mt-0 mt-5 w-0 h-full invisible transition-all duration-500  delay-150"
+          ?"sm:relative fixed flex flex-col justify-start bg-amber-200 sm:row-start-2 sm:col-start-1 sm:col-span-1 sm:mt-0 mt-4 sm:w-full w-24 h-full visible transition-all duration-500 "
+          :"sm:relative fixed flex flex-col justify-start bg-amber-200 sm:row-start-2 sm:col-start-1 sm:col-span-1 sm:mt-0 mt-4 w-0 h-full invisible transition-all duration-500  delay-150"
         }>
           <Link className={menuButton ?"opacity-100 visible transition-all ease-in-out duration-300 delay-150":"opacity-0 invisible transition-all ease-in-out duration-300"} to=''>Reset</Link> 
           <Link className={menuButton ?"opacity-100 visible transition-all ease-in-out duration-300 delay-150":"opacity-0 invisible transition-all ease-in-out duration-300"} to='Categ1'>Categ1</Link>
-          <button className="visible" onClick={()=>setMenuButton(!menuButton)}>CERRAR Y ABRIR MENU</button>
+          <button className="sm:invisible visible" onClick={()=>setMenuButton(!menuButton)}>CERRAR Y ABRIR MENU</button>
         </div>
         {/* Catalogo */}
-        <div className='sm:bg-amber-400 row-start-2 sm:col-start-2 sm:col-span-7 col-start-3 col-end-8 min-h-screen max-h-fit'>
+        <div className="sm:bg-amber-400 bg-amber-400 row-start-2 sm:col-start-2 sm:col-span-7 col-start-3 col-end-8 min-h-screen max-h-fit">
           <Outlet/>
         </div>
     </motion.div>

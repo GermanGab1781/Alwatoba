2d651e4b7ea0692288f708e90c6033668a924342
unknown <germangabriel1998@gmail.com>
2022-08-18 18:26:23 -0300

    Added mobile view for Producto Page

--- src/pages/ProductEj.jsx
+++ src/pages/ProductEj.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import {motion} from 'framer-motion'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import AlwatobaImg from '../media/AlwatobaBrand.png'
 import { Carousel } from 'react-responsive-carousel';
 import Viewer from 'react-viewer'
 
@@ -18,11 +17,11 @@ export default function ProductEj() {
     setShowViewer(true);
   }
   return (
-    <motion.div className="grid grid-cols-5 bg-amber-500 pt-20 text-center" initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
+    <motion.div className="grid grid-cols-6 bg-amber-500 pt-20 text-center" initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
       {/* Titulo */}
-      <span className="row-start-1 col-span-6">NOMBRE DEL PRODUCTO</span>
+      <span className="row-start-1 col-span-6 text-2xl font-semibold mb-10">NOMBRE DEL PRODUCTO</span>
       {/* Viewer */}
-      <div className="row-start-2 col-start-2 col-span-4 w-3/5 h-full">
+      <div className="row-start-2 w-full col-start-2 sm:col-end-5 col-end-6 h-full border border-black">
         <Carousel showThumbs={false} infiniteLoop={true} emulateTouch={true}>
         {imagesUrls.map((img,index)=>{
           return(
@@ -32,18 +31,17 @@ export default function ProductEj() {
             </div>  
           )
         })}
-        </Carousel>
-        
+        </Carousel>      
       </div>
       {/* Botones */}
-      <div className="flex flex-col row-start-2 col-start-6 place-content-center ml-5">
-        <button className="p-5 border border-neutral-800">Scroll test right</button>
-        <button className="p-5 border border-neutral-800">Scroll test left</button> 
-        <button className="p-5 border border-neutral-800">Boton3</button> 
-        <button className="p-5 border border-neutral-800">Boton4</button> 
+      <div className="flex flex-col sm:col-start-5 row-start-2 col-start-6 sm:gap-y-2 gap-y-3 place-content-center sm:ml-10 ml-1 sm:-mr-10 mr-4">
+        <button className="sm:py-5 py-2 sm:text-base text-xs font-bold border border-neutral-800">Boton1</button>
+        <button className="sm:py-5 py-2 sm:text-base text-xs font-bold border border-neutral-800">Boton2</button> 
+        <button className="sm:py-5 py-2 sm:text-base text-xs font-bold border border-neutral-800">Boton3</button> 
+        <button className="sm:py-5 py-2 sm:text-base text-xs font-bold border border-neutral-800">Boton4</button> 
       </div>
       {/* Descripcion */}
-      <div className="row-start-3 col-start-2 col-end-6 mb-5 mt-10">
+      <div className="row-start-3 sm:col-start-2 sm:col-end-6 col-start-1 col-end-7 mb-5 mt-10">
         <p className="border border-black text-lg font-semibold">DESCRIPCION</p>
         <p className="border border-black">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
       </div>

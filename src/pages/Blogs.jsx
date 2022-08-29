348996bfe687e42f78c70a0f747df318ffca6ee9
unknown <germangabriel1998@gmail.com>
2022-08-29 20:35:49 -0300

    More style to Blog Page and Home page adjustments

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -3,15 +3,19 @@ import React from 'react'
 
 export default function Blogs() {
   return (
-    <motion.div className="grid pt-24">
+    <motion.div className="grid pt-24 custom-ColorMain">
       {/* Titulo */}
-      <span className="row-start-1 text-center text-4xl font-bold">Vanguardia de la Artesania Provincial</span>
-      {/* Descripcion */}
-      <span className="row-start-2 text-xl">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</span>
-      {/* Imagen 1 */}
-      
-      {/* Imagen 2 */}
-      
+      <span className="row-start-1 text-center text-4xl font-bold pb-4">Vanguardia en Artesania Provincial</span>
+      <div className="grid relative row-start-2 h-fit border custom-BorderColorMain rounded-xl p-4 mx-1 my-1">  
+        {/* SubTitulo 1 */}      
+        <span className="row-start-1 text-3xl underline">Subtitulo1</span>
+        {/* Texto 1 */}
+        <span className="row-start-2 row-span-1 h-full text-2xl ">Uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). alt="blogEjemplo"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). alt="blogEjemplo"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). alt="blogEjemplo"</span>
+        {/* Imagen 1 */} 
+        <img className="row-start-2 row-span-1 w-full h-full col-span-1 rounded-xl" src={require("../media/BlogEj/Blog1.jpg")} alt="blogEjemplo"/>
+      </div>
+      {/* Imagen Final */}
+      <img className="row-start-3 w-screen h-full p-3" src={require("../media/BlogEj/Blog2.jpg")} alt="blogEjemplo"/>
     </motion.div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,11 @@ import { NavLink } from 'react-router-dom'
 import AlwatobaImg from '../media/AlwatobaBrand.png'
 
 export default function Home() {
-  const imagesUrls = [{original:require("../media/GaleriaEj/Galeria1.jpg"),alt:"ImagenPrueba1",originalClass:"w-96 h-96 cursor-default"},
-                      {original:require("../media/GaleriaEj/Galeria2.jpg"),alt:"ImagenPrueba2",originalClass:"w-96 h-96 cursor-default"},
-                      {original:require("../media/GaleriaEj/Galeria4.jpg"),alt:"ImagenPrueba4",originalClass:"w-96 h-96 cursor-default"},
-                      {original:require("../media/GaleriaEj/Galeria5.jpg"),alt:"ImagenPrueba5",originalClass:"w-96 h-96 cursor-default"},
-                      {original:require("../media/GaleriaEj/Galeria6.jpg"),alt:"ImagenPrueba6",originalClass:"w-96 h-96 cursor-default"}
+  const imagesUrls = [{original:require("../media/GaleriaEj/Galeria1.jpg"),alt:"ImagenPrueba1",originalClass:"xl:w-96 xl:h-96 sm:w-64 w-48 h-48 cursor-default"},
+                      {original:require("../media/GaleriaEj/Galeria2.jpg"),alt:"ImagenPrueba2",originalClass:"xl:w-96 xl:h-96 sm:w-64 w-48 h-48 cursor-default"},
+                      {original:require("../media/GaleriaEj/Galeria4.jpg"),alt:"ImagenPrueba4",originalClass:"xl:w-96 xl:h-96 sm:w-64 w-48 h-48 cursor-default"},
+                      {original:require("../media/GaleriaEj/Galeria5.jpg"),alt:"ImagenPrueba5",originalClass:"xl:w-96 xl:h-96 sm:w-64 w-48 h-48 cursor-default"},
+                      {original:require("../media/GaleriaEj/Galeria6.jpg"),alt:"ImagenPrueba6",originalClass:"xl:w-96 xl:h-96 sm:w-64 w-48 h-48 cursor-default"}
   ]
   function clickEvHome() {
     window.scrollTo(0, 0);
@@ -30,15 +30,15 @@ export default function Home() {
       {/* Collage fotos */}
       <div className="row-start-3 xl:col-start-2 xl:col-span-4 col-start-1 col-span-6 mb-16">
         <div className="grid gap-x-3 gap-y-2">
-          <img className="xl:col-start-1 xl:col-span-1 xl:row-start-1 col-start-1 col-span-2 row-start-1 w-60 h-60 custom-BgColor2 border custom-BorderColor3 xl:p-3 p-2 rounded-2xl"src={require("../media/GaleriaEj/Galeria7.jpg")} alt={"imagenPrueba"}/>
-          <img className="xl:col-start-1 xl:col-span-1 xl:row-start-2 col-start-3 col-span-2 row-start-1 w-60 h-60 custom-BgColor2 border custom-BorderColor3 xl:p-3 p-2 rounded-2xl"src={require("../media/GaleriaEj/Galeria10.jpg")} alt={"imagenPrueba"}/>
+          <img className="xl:col-start-1 xl:col-span-1 xl:row-start-1 col-start-1 col-span-2 row-start-1 sm:w-60 sm:h-60 w-40 h-40 custom-BgColor2 border custom-BorderColor3 xl:p-3 p-2 rounded-2xl"src={require("../media/GaleriaEj/Galeria7.jpg")} alt={"imagenPrueba"}/>
+          <img className="xl:col-start-1 xl:col-span-1 xl:row-start-2 col-start-3 col-span-2 row-start-1 sm:w-60 sm:h-60 w-40 h-40 custom-BgColor2 border custom-BorderColor3 xl:p-3 p-2 rounded-2xl"src={require("../media/GaleriaEj/Galeria10.jpg")} alt={"imagenPrueba"}/>
 
-          <div className="xl:col-start-2 xl:col-span-1 xl:row-start-1 col-start-1 col-span-4 row-start-2 xl:row-span-2 custom-BgColor2 border custom-BorderColor3 xl:m-0 m-auto xl:p-8 p-3 rounded-2xl sm:w-fit sm:h-fit w-96 h-fit">
+          <div className="xl:col-start-2 xl:col-span-1 xl:row-start-1 col-start-1 col-span-4 row-start-2 xl:row-span-2 custom-BgColor2 border custom-BorderColor3 xl:m-0 m-auto xl:p-8 p-3 rounded-2xl sm:w-fit sm:h-fit w-fit h-fit">
             <ReactImageGallery items={imagesUrls} showFullscreenButton={false} showNav={false} showPlayButton={false} autoPlay={true}/>
           </div>
 
-          <img className="xl:col-start-3 xl:col-span-1 xl:row-start-1 col-start-1 col-span-2 row-start-3 w-60 h-60 custom-BgColor2 border custom-BorderColor3 xl:p-3 p-2 rounded-2xl" src={require("../media/GaleriaEj/Galeria8.jpg")} alt={"imagenPrueba"}/>
-          <img className="xl:col-start-3 xl:col-span-1 xl:row-start-2 col-start-3 col-span-2 row-start-3 w-60 h-60 custom-BgColor2 border custom-BorderColor3 xl:p-3 p-2 rounded-2xl" src={require("../media/GaleriaEj/Galeria3.jpg")} alt={"imagenPrueba"}/>
+          <img className="xl:col-start-3 xl:col-span-1 xl:row-start-1 col-start-1 col-span-2 row-start-3 sm:w-60 sm:h-60 w-40 h-40 custom-BgColor2 border custom-BorderColor3 xl:p-3 p-2 rounded-2xl" src={require("../media/GaleriaEj/Galeria8.jpg")} alt={"imagenPrueba"}/>
+          <img className="xl:col-start-3 xl:col-span-1 xl:row-start-2 col-start-3 col-span-2 row-start-3 sm:w-60 sm:h-60 w-40 h-40 custom-BgColor2 border custom-BorderColor3 xl:p-3 p-2 rounded-2xl" src={require("../media/GaleriaEj/Galeria3.jpg")} alt={"imagenPrueba"}/>
           <NavLink className="xl:row-start-4 xl:col-start-2 xl:col-span-1 row-start-4 col-start-1 col-span-4 text-center custom-BgColor2 font-semibold hover:scale-110 border custom-BorderColor2 xl:p-3 p-5 rounded-xl transition-all" onClick={()=>clickEvHome()} to="/Catalog"><span className="">VER CATÁLOGO</span></NavLink>
         </div>
         

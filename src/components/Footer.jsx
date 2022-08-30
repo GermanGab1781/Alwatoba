73f0952111b86a4c6b1dcfd12f72fd1593f31312
unknown <germangabriel1998@gmail.com>
2022-08-30 01:47:49 -0300

    Home Page Colors and styles Changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,14 @@ import {BsInstagram , BsWhatsapp} from 'react-icons/bs'
 import {BiCopyright} from 'react-icons/bi'
 export default function Footer() {
   return (
-    <motion.div className="grid justify-center p-6 pt-20 custom-BgColorMain custom-Color1 transition-all border">
+    <motion.div className="grid justify-center p-6 pt-20 custom-BgColor1 custom-ColorMain transition-all border">
       <div className="flex flex-row row-start-1 place-content-center">
         <a  href="https://www.instagram.com/alwatoba"><BsInstagram className="h-10 w-20 hover:scale-110 transition-all"/></a>
         <a  href="https://www.instagram.com/alwatoba"><BsWhatsapp className="h-10 w-20 hover:scale-110 transition-all"/></a>
       </div>
       <div className="flex flex-wrap row-start-2 mt-5 place-content-center">
         <BiCopyright/> 
-        <span className="text-lg">Alwatoba 2018</span>
+        <span className="text-lg custom-Color3">Alwatoba 2018</span>
       </div>
     </motion.div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,37 +16,39 @@ export default function Home() {
     window.scrollTo(0, 0);
   }
   return (
-    <motion.div className="custom-BgColorMain grid gap-y-10 grid-cols-6 pt-24 place-items-center " initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
+    <motion.div className="grid bg-slate-50 gap-y-10 grid-cols-6 pt-24 place-items-center" initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
       
       {/* Presentacion */}
-      <div className="row-start-1 col-start-3 col-span-2 custom-BgColor2 border custom-BorderColor2 p-5 rounded-2xl">
+      <div className="row-start-1 col-start-3 col-span-2 custom-BgColorMain border custom-BorderColor2 p-5 rounded-2xl">
         <ReactPlayer width={"80vw"} height={"35vw"} url='www.youtube.com/watch?v=cFPkypL_ap8'/>
       </div>
       {/* Frase 1 */}
       <div className="relative row-start-2 col-start-3 col-span-2 h-32 w-1/2">
         <img className="h-full w-full rounded-full" src={AlwatobaImg} alt="Imagen fondo"/>
-        <span className="absolute custom-BgColor2 bottom-1/3 left-1/2 -translate-x-1/2 whitespace-nowrap sm:text-2xl text-xs font-bold custom-ColorMain">"Conscientes de nuestro impacto en la Cultura"</span>
+        <span className="absolute custom-BgColorMain bottom-1/3 left-1/2 -translate-x-1/2 whitespace-nowrap sm:text-2xl text-xs font-bold custom-Color2">"Conscientes de nuestro impacto en la Cultura"</span>
       </div>
       {/* Collage fotos */}
-      <div className="row-start-3 xl:col-start-2 xl:col-span-4 col-start-1 col-span-6 mb-16">
+      <div className="custom-BgColor4 col-span-6 row-start-3 w-full xl:py-20 py-10 mb-16">
+        
         <div className="grid gap-x-3 gap-y-2">
-          <img className="xl:col-start-1 xl:col-span-1 xl:row-start-1 col-start-1 col-span-2 row-start-1 sm:w-60 sm:h-60 w-40 h-40 custom-BgColor2 border custom-BorderColor3 xl:p-3 p-2 rounded-2xl"src={require("../media/GaleriaEj/Galeria7.jpg")} alt={"imagenPrueba"}/>
-          <img className="xl:col-start-1 xl:col-span-1 xl:row-start-2 col-start-3 col-span-2 row-start-1 sm:w-60 sm:h-60 w-40 h-40 custom-BgColor2 border custom-BorderColor3 xl:p-3 p-2 rounded-2xl"src={require("../media/GaleriaEj/Galeria10.jpg")} alt={"imagenPrueba"}/>
+          <span className="xl:col-start-1 xl:col-span-6 xl:row-start-1 col-start-1 col-span-5 row-start-1 text-3xl custom-ColorMain text-center mb-10">INDUMENTARIA</span>
+          <img className="xl:col-start-1 xl:col-span-2 xl:row-start-2 m-auto col-start-1 col-span-2 row-start-2 sm:w-60 sm:h-60 w-40 h-40 xl:p-3 p-2 custom-BgColorMain border custom-BorderColor3 rounded-2xl"src={require("../media/GaleriaEj/Galeria7.jpg")} alt={"imagenPrueba"}/>
+          <img className="xl:col-start-1 xl:col-span-2 xl:row-start-3 m-auto col-start-4 col-span-2 row-start-2 sm:w-60 sm:h-60 w-40 h-40 xl:p-3 p-2 custom-BgColorMain border custom-BorderColor3 rounded-2xl"src={require("../media/GaleriaEj/Galeria10.jpg")} alt={"imagenPrueba"}/>
 
-          <div className="xl:col-start-2 xl:col-span-1 xl:row-start-1 col-start-1 col-span-4 row-start-2 xl:row-span-2 custom-BgColor2 border custom-BorderColor3 xl:m-0 m-auto xl:p-8 p-3 rounded-2xl sm:w-fit sm:h-fit w-fit h-fit">
+          <div className="xl:col-start-3 xl:col-span-2 xl:row-start-2 col-start-1 col-end-6 row-start-3 xl:row-span-2 custom-BgColorMain border custom-BorderColor3 m-auto xl:p-8 p-3 rounded-2xl sm:w-fit sm:h-fit w-fit h-fit">
             <ReactImageGallery items={imagesUrls} showFullscreenButton={false} showNav={false} showPlayButton={false} autoPlay={true}/>
           </div>
 
-          <img className="xl:col-start-3 xl:col-span-1 xl:row-start-1 col-start-1 col-span-2 row-start-3 sm:w-60 sm:h-60 w-40 h-40 custom-BgColor2 border custom-BorderColor3 xl:p-3 p-2 rounded-2xl" src={require("../media/GaleriaEj/Galeria8.jpg")} alt={"imagenPrueba"}/>
-          <img className="xl:col-start-3 xl:col-span-1 xl:row-start-2 col-start-3 col-span-2 row-start-3 sm:w-60 sm:h-60 w-40 h-40 custom-BgColor2 border custom-BorderColor3 xl:p-3 p-2 rounded-2xl" src={require("../media/GaleriaEj/Galeria3.jpg")} alt={"imagenPrueba"}/>
-          <NavLink className="xl:row-start-4 xl:col-start-2 xl:col-span-1 row-start-4 col-start-1 col-span-4 text-center custom-BgColor2 font-semibold hover:scale-110 border custom-BorderColor2 xl:p-3 p-5 rounded-xl transition-all" onClick={()=>clickEvHome()} to="/Catalog"><span className="">VER CATÁLOGO</span></NavLink>
+          <img className="xl:col-start-5 xl:col-span-2 xl:row-start-2 m-auto col-start-1 col-span-2 row-start-4 sm:w-60 sm:h-60 w-40 h-40 xl:p-3 p-2 custom-BgColorMain border custom-BorderColor3 rounded-2xl" src={require("../media/GaleriaEj/Galeria8.jpg")} alt={"imagenPrueba"}/>
+          <img className="xl:col-start-5 xl:col-span-2 xl:row-start-3 m-auto col-start-4 col-span-2 row-start-4 sm:w-60 sm:h-60 w-40 h-40 xl:p-3 p-2 custom-BgColorMain border custom-BorderColor3 rounded-2xl" src={require("../media/GaleriaEj/Galeria3.jpg")} alt={"imagenPrueba"}/>
+          <NavLink className="xl:col-start-2 xl:col-span-4 xl:row-start-4 row-start-5 col-start-1 col-span-5 text-center custom-BgColor2 xl:text-2xl font-semibold hover:scale-110 border custom-BorderColor2 xl:mt-10 mt-5 xl:p-5 p-5 rounded-xl transition-all" onClick={()=>clickEvHome()} to="/Catalog"><span className="">VER CATÁLOGO</span></NavLink>
         </div>
         
       </div>      
       {/* Frase 2 */}
       <div className="relative row-start-5 col-start-3 col-span-2 h-32 w-1/2">
         <img className="h-full w-full rounded-full" src={AlwatobaImg} alt="Imagen fondo"/>
-        <span className="absolute custom-BgColor2 bottom-1/3 left-1/2 -translate-x-1/2 whitespace-nowrap sm:text-2xl text-xs font-bold custom-ColorMain">"Trabajando de la mano de las comunidades locales"</span>
+        <span className="absolute custom-BgColorMain bottom-1/3 left-1/2 -translate-x-1/2 whitespace-nowrap sm:text-2xl text-xs font-bold custom-Color2">"Trabajando de la mano de las comunidades locales"</span>
       </div>
 
     </motion.div>

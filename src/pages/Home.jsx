a443b90ec65d8d04e7137404790bb259e6b777c1
unknown <germangabriel1998@gmail.com>
2022-10-18 21:45:58 -0300

    Home Page style changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,39 +12,53 @@ export default function Home() {
                       {original:require("../media/GaleriaEj/Galeria6.jpg"),alt:"ImagenPrueba6",originalClass:"xl:w-96 xl:h-96 sm:w-64 w-48 h-48 cursor-default"}
   ]
   useEffect(() => {
-    window.scrollTo(0, 0);
+    //window.scrollTo(0, 0);
   },[])
   return (
-    <motion.div className="grid bg-slate-100 md:gap-y-4 gap-y-5 grid-cols-6 md:pt-15 pt-20 place-items-center" initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
-      
+    <motion.div className="grid bg-slate-100 md:gap-y-4 gap-y-5 md:pt-15 pt-20 place-items-center" initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
+ 
       {/* Presentacion */}
-      <div className="grid row-start-1 col-start-1 col-span-6 text-center">
-        <div className="flex flex-col" >
+      <div className="flex flex-col text-center">
           <span className="font-bold pb-4 pt-1 xl:text-8xl text-6xl font-comfortaa">Alwatoba</span>
           <span className="font-semibold pt-2 xl:text-2xl text-1xl custom-ColorMain font-notethis">Quienes somos y que nos inspira</span>
-          <div className="">
             <ReactPlayer width='60vw' height='27vw' url='www.youtube.com/watch?v=cFPkypL_ap8'/>
-          </div>
-          <NavLink className="mt-2 text-center md:text-3xl text-2xl custom-Color4 hover:scale-110 hover:font-bold font-notethis transition-all delay-75 " to="/Blogs">Leer más</NavLink>
+          <NavLink className="mt-2 text-center md:text-3xl text-2xl custom-Color4 hover:font-bold font-notethis transition-all delay-75 " to="/Blogs">Leer más</NavLink>
         </div>
-
-      </div>
       {/* Collage fotos */}
-      <div className="custom-BgColor4 col-span-6 row-start-3 w-full xl:py-20 py-10 mb-5">
-        <div className="grid gap-x-3 gap-y-2">
-          <span className="xl:col-start-1 xl:col-span-6 xl:row-start-1 col-start-1 col-span-5 row-start-1  xl:text-8xl md:text-7xl sm:text-6xl text-5xl custom-ColorMain text-center mb-10 font-comfortaa">Indumentaria</span>
-          <img className="xl:col-start-1 xl:col-span-2 xl:row-start-2 m-auto col-start-1 col-span-2 row-start-2 sm:w-60 sm:h-60 w-40 h-40 xl:p-3 p-2 shadow-lg shadow-black rounded-2xl"src={require("../media/GaleriaEj/Galeria7.jpg")} alt={"imagenPrueba"}/>
-          <img className="xl:col-start-1 xl:col-span-2 xl:row-start-3 m-auto col-start-4 col-span-2 row-start-2 sm:w-60 sm:h-60 w-40 h-40 xl:p-3 p-2 shadow-lg shadow-black rounded-2xl"src={require("../media/GaleriaEj/Galeria10.jpg")} alt={"imagenPrueba"}/>
+      <div className='row-start-2 custom-BgColor4 w-full text-center py-10 my-10'>
+        <span className="xl:text-8xl md:text-7xl sm:text-6xl text-5xl custom-ColorMain text-center font-comfortaa">Indumentaria</span>
+        <div className='flex flex-row place-content-center mt-5 lg:gap-x-7 xl:gap-x-10 2xl:gap-x-32 gap-x-2'>
 
-          <div className="xl:col-start-3 xl:col-span-2 xl:row-start-2 col-start-1 col-end-6 row-start-3 xl:row-span-2 shadow-lg shadow-black m-auto xl:p-8 p-3 rounded-2xl sm:w-fit sm:h-fit w-fit h-fit">
-            <ReactImageGallery items={imagesUrls} showFullscreenButton={false} showNav={false} showPlayButton={false} autoPlay={true}/>
+          <div className=''>
+            <div>
+              <img className='md:w-60 md:h-60 h-32 w-32 shadow-lg shadow-black md:border-r-0 md:border-b border-b-0 border custom-BorderColorMain m-auto' src={require("../media/GaleriaEj/Galeria7.jpg")} alt={"imagenPrueba"}/>
+              <div className='flex md:flex-row flex-col md:mt-10 border custom-BorderColorMain md:border-t border-t-0 shadow-lg shadow-black'>
+                <img className='md:w-52 md:h-52 h-32 w-32' src={require("../media/GaleriaEj/Galeria3.jpg")} alt={"imagenPrueba"}/>
+                <img className='md:w-52 md:h-52 h-32 w-32' src={require("../media/GaleriaEj/Galeria10.jpg")} alt={"imagenPrueba"}/>
+              </div>
+            </div>
           </div>
 
-          <img className="xl:col-start-5 xl:col-span-2 xl:row-start-2 m-auto col-start-1 col-span-2 row-start-4 sm:w-60 sm:h-60 w-40 h-40 xl:p-3 p-2 shadow-lg shadow-black rounded-2xl" src={require("../media/GaleriaEj/Galeria8.jpg")} alt={"imagenPrueba"}/>
-          <img className="xl:col-start-5 xl:col-span-2 xl:row-start-3 m-auto col-start-4 col-span-2 row-start-4 sm:w-60 sm:h-60 w-40 h-40 xl:p-3 p-2 shadow-lg shadow-black rounded-2xl" src={require("../media/GaleriaEj/Galeria3.jpg")} alt={"imagenPrueba"}/>
-          <NavLink className="xl:col-start-1 xl:col-span-6 xl:row-start-4 row-start-5 col-start-1 col-span-5 m-auto mt-6 shadow-xl shadow-black hover:scale-110 transition-all" to="/Catalog/All"><span className="text-center custom-BgColor1 custom-ColorMain xl:text-2xl font-semibold border custom-BorderColor2 xl:mt-10 mt-5 xl:p-5 p-5 rounded-xl font-comfortaa">VER CATÁLOGO</span></NavLink>
+          <div className='my-auto shadow-lg shadow-black border custom-BorderColorMain border-x-0'>
+            <ReactImageGallery className='' items={imagesUrls} showFullscreenButton={false} showNav={false} showPlayButton={false} autoPlay={true}/>
+          </div>
+
+          <div className=''>
+            <div>
+              <img className='md:w-60 md:h-60 h-32 w-32 shadow-lg shadow-black md:border-l-0 md:border-b border-b-0 border custom-BorderColorMain m-auto ' src={require("../media/GaleriaEj/Galeria8.jpg")} alt={"imagenPrueba"}/>
+              <div className='flex md:flex-row flex-col md:mt-10 border custom-BorderColorMain md:border-t border-t-0 shadow-lg shadow-black'>
+                <img className='md:w-52 md:h-52 h-32 w-32' src={require("../media/GaleriaEj/Galeria9.jpg")} alt={"imagenPrueba"}/>
+                <img className='md:w-52 md:h-52 h-32 w-32' src={require("../media/GaleriaEj/Galeria8.jpg")} alt={"imagenPrueba"}/>
+              </div>
+            </div>
+          </div>
+
         </div>
-      </div>      
+        <div className='mt-10'>
+          <NavLink className="hover:shadow-xl hover:shadow-black text-center custom-BgColor1 custom-ColorMain xl:text-2xl font-semibold  xl:mt-10 mt-5 xl:p-5 p-5 rounded-xl font-comfortaa" to="/Catalog/All">VER CATÁLOGO</NavLink>
+        </div>
+      </div>
+
     </motion.div>
   )
 }

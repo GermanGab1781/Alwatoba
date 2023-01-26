5c7379b77b3d35e15187c7567f57c87d95e55a9f
Germán Gabriel <101677477+GermanGab1781@users.noreply.github.com>
2023-01-26 10:21:08 -0300

    Changes to slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,75 +20,34 @@ export default function Home() {
 
   function FirstSlide() {
     if (!isMobile) {
-      return <motion.div className='relative cursor-default custom-ColorMain custom-ColorMain custom-minH90'>
-        <img src={SvgImg2} alt="Cargando Imagen" style={{ height: "100%" }} className='cursor-default w-screen relative'></img>
-        <motion.span
-          className='absolute flex flex-col gap-y-16 bottom-1/4 right-10 transform -translate-x-1/2 -translate-y-1/2 p-5 h-fit w-fit'
-          initial={{ opacity: 0, y: -800 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.6, duration: 0.9 }}
-        >
-        </motion.span>
-      </motion.div>
+      return <motion.div style={{minHeight:"90vh",height:"90vh"}} className='relative cursor-default custom-ColorMain custom-ColorMain w-screen'>
+        <img src={SvgImg2} alt="Primer Slide de la presentacion" className='object-cover h-full w-full'/>
+      </motion.div> 
     }
-    return <motion.div className='relative cursor-default custom-ColorMain custom-minH90'>
-      <img src={SvgImg2} alt="Cargando Imagen" style={{ height: "100%" }} className='cursor-default w-screen relative'></img>
-      <motion.span
-        className='absolute flex flex-col gap-y-10 transform bottom-5 right-7 -translate-y-1/2 '
-        initial={{ opacity: 0, y: -300 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.6, duration: 0.9 }}>
-        
-      </motion.span>
+    return <motion.div className='relative cursor-default custom-ColorMain '>
+      <img src={SvgImg2} alt="Primer Slide de la presentacion" className='object-cover h-full w-full'></img>
     </motion.div>
   }
 
   function SecondSlide() {
     if (!isMobile) {
-      return <motion.div className='relative cursor-default custom-ColorMain custom-ColorMain custom-minH90'>
-        <img src={SvgImg} alt="Cargando Imagen" style={{ height: "100%" }} className='cursor-default w-screen relative'></img>
-        <motion.span
-          className='absolute flex flex-col gap-y-16 bottom-1/4 right-10 transform -translate-x-1/2 -translate-y-1/2 p-5 h-fit w-fit'
-          initial={{ opacity: 0, y: -800 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.6, duration: 0.9 }}
-        >
-        </motion.span>
+      return <motion.div style={{minHeight:"90vh",height:"90vh"}} className='relative cursor-default custom-ColorMain custom-ColorMain w-screen'>
+        <img src={SvgImg} alt="Segundo Slide de la presentacion" className='object-cover h-full w-full'></img>
       </motion.div>
     }
     return <motion.div className='relative cursor-default custom-ColorMain custom-minH90'>
-      <img src={SvgImg} alt="Cargando Imagen" style={{ height: "100%" }} className='cursor-default w-screen relative'></img>
-      <motion.span
-        className='absolute flex flex-col gap-y-10 transform bottom-5 right-7 -translate-y-1/2 '
-        initial={{ opacity: 0, y: -300 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.6, duration: 0.9 }}>
-        
-      </motion.span>
+      <img src={SvgImg} alt="Segundo Slide de la presentacion" className='object-cover h-full w-full'></img>
     </motion.div>
   }
 
   function ThirdSlide() {
     if (!isMobile) {
-      return <motion.div className='relative cursor-default custom-ColorMain custom-ColorMain custom-minH90'>
-        <img src={SvgImg3} alt="Cargando Imagen" style={{ height: "100%" }} className='cursor-default w-screen relative'></img>
-        <motion.span
-          className='absolute flex flex-col gap-y-16 bottom-1/4 right-10 transform -translate-x-1/2 -translate-y-1/2 p-5 h-fit w-fit'
-          initial={{ opacity: 0, y: -800 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.6, duration: 0.9 }}
-        >
-        </motion.span>
+      return <motion.div style={{minHeight:"90vh",height:"90vh"}} className='relative cursor-default custom-ColorMain custom-ColorMain w-screen'>
+        <img src={SvgImg3} alt="Tercer Slide de la presentacion" className='object-cover h-full w-full'></img>
       </motion.div>
     }
     return <motion.div className='relative cursor-default custom-ColorMain custom-minH90'>
-      <img src={SvgImg3} alt="Cargando Imagen" style={{ height: "100%" }} className='cursor-default w-screen relative'></img>
-      <motion.span
-        className='absolute flex flex-col gap-y-10 transform bottom-5 right-7 -translate-y-1/2 '
-        initial={{ opacity: 0, y: -300 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.6, duration: 0.9 }}>
-      </motion.span>
+      <img src={SvgImg3} alt="Tercer Slide de la presentacion" className='object-cover h-full w-full'></img>
     </motion.div>
   }
   const imagesUrls2 = [
@@ -101,7 +60,7 @@ export default function Home() {
     window.scrollTo(0, 0);
   }, [])
   return (
-    <motion.div className="grid bg-slate-100 md:gap-y-4 gap-y-5 md:pt-15 pt-16 place-items-center " initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+    <motion.div className="grid bg-slate-100 md:gap-y-4 gap-y-5 md:pt-15 pt-16 pb-10 place-items-center " initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
       {/* Main Slider */}
       <ReactImageGallery items={imagesUrls2} showThumbnails={false} showFullscreenButton={false} showPlayButton={true} slideInterval={9000} autoPlay={false} additionalClass={''} />
     </motion.div>
